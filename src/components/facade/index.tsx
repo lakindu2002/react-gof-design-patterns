a84d43ed9823140963a0eb9ff305460ce24b207c
lakindu hewawasam <lakindu2002@live.com>
2022-04-20 20:59:19 +0530

    added facade

---
+++ src/components/facade/index.tsx
@@ -0,0 +1,43 @@
+import axios from "axios";
+import { FC, useCallback, useEffect, useState } from "react";
+import { useFacadeUserAPI } from "../../hooks/use-facade-user-api";
+
+export const UserTable = (props: any) => {
+    console.log(props.users, props.onDelete);
+    return <></>;
+};
+
+export const UserCreateModal = (props: any) => {
+    console.log(props.onCreate);
+    return <></>;
+};
+
+export const NoFacade: FC = () => {
+    const userFacade = useFacadeUserAPI();
+    const { createUser, deleteUser, getUsers, users } = userFacade;
+
+    const fetchUsers = useCallback(async () => {
+        // replace with facade API method to simplify code
+        await getUsers();
+    }, [getUsers]);
+
+    useEffect(() => {
+        fetchUsers();
+    }, [fetchUsers]);
+
+    const handleUserDelete = async (id: string) => {
+        // replace with Facade method to hide complex code required in deleting
+        await deleteUser(id);
+    };
+
+    const handleCreateUser = async (user: any) => {
+        // replace with a facade method to hide complex code required in creating
+        await createUser(user);
+    };
+    return (
+        <>
+            <UserTable users={users} onDelete={handleUserDelete} />
+            <UserCreateModal onCreate={handleCreateUser} />
+        </>
+    );
+};
--- src/hooks/use-facade-user-api.tsx
+++ src/hooks/use-facade-user-api.tsx
@@ -1,3 +1,51 @@
-export const useFacadeUserAPI = () => {
+import axios from "axios";
+import { useState } from "react";
 
-}
+export const useFacadeUserAPI = () => {
+  const [users, setUsers] = useState<any>([]);
+  const [actionExecuting, setActionExecuting] = useState<boolean>(false);
+
+  async function getUsers() {
+    setActionExecuting(true);
+    try {
+      const resp = await axios.get("/api/users");
+      setUsers(resp.data);
+    } catch (err) {
+      console.log(err);
+    } finally {
+      setActionExecuting(false);
+    }
+  }
+
+  async function createUser(user: any) {
+    setActionExecuting(true);
+    try {
+      await axios.post("/api/users", user);
+      setUsers([...users, user]);
+    } catch (err) {
+      console.log(err);
+    } finally {
+      setActionExecuting(false);
+    }
+  }
+
+  async function deleteUser(id: string) {
+    setActionExecuting(true);
+    try {
+      await axios.delete(`/api/users/${id}`);
+      setUsers(users.filter((user: any) => user.id !== id));
+    } catch (err) {
+      console.log(err);
+    } finally {
+      setActionExecuting(false);
+    }
+  }
+
+  return {
+    users,
+    actionExecuting,
+    getUsers,
+    createUser,
+    deleteUser,
+  };
+};
